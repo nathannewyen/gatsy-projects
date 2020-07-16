40b416e4560ee0bb6591e019b539fbfb3582b32c
Nhan Nguyen <nathan13574@gmail.com>
2020-07-16 01:40:46 -0500

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati fugiat blanditiis quisquam hic
+				perspiciatis nam, ea expedita sit harum beatae ullam quis adipisci doloribus, necessitatibus totam unde.
+				Quos, rerum exercitationem.
+			</p>
+		</div>
+	);
+};
+export default About;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+	<Layout>
+		<SEO title="Nhan Nguyen" />
+		<h1>Nhan Nguyen Portfolio</h1>
+		<p>Welcome to your new Gatsby site.</p>
+		<p>Now go build something great.</p>
+	</Layout>
+);
+
+export default IndexPage;
